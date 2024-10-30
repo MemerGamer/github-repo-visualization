2aaed7a9c8961ac594fcf7f051ed28e9c7e1decc
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-10-30 23:43:01 +0200

    feat: added custom routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GitHubUserSearch from './components/GitHubUserSearch';
+import NotFound from './components/NotFound';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/:usernameParam?/:layoutParam?" element={<GitHubUserSearch />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
